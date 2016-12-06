15c11d38f8d6281535c8b7f1a43486e7f67d946d
jackal998 <jackal998@gmail.com>
2016-12-06 19:13:39 +0800

    map_partial

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :events do
+    collection do
+      get :match
+    end
+  end
   root :to => "events#index"
 end
