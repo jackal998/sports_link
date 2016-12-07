0d4957c93b0c2ccde66b5d5d5da2d45df3ae39e4
jackal998 <jackal998@gmail.com>
2016-12-07 11:42:29 +0800

    create_coord_migrate

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,4 +2,7 @@ class EventsController < ApplicationController
   def index
     @events = Event.all
   end
+  def match
+    byebug
+  end
 end
---
+++ db/migrate/20161206112018_add_place_coord.rb
@@ -0,0 +1,5 @@
+class AddPlaceCoord < ActiveRecord::Migration[5.0]
+  def change
+    # add_column :places, :
+  end
+end
