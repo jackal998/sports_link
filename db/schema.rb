89beed36cd99ce8f69c9678f648071775c1a8e9a
jackal998 <jackal998@gmail.com>
2016-12-11 15:26:56 +0800

    install_devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161210094349) do
+ActiveRecord::Schema.define(version: 20161211072520) do
 
   create_table "events", force: :cascade do |t|
     t.string   "sport_name"
@@ -32,6 +32,23 @@ ActiveRecord::Schema.define(version: 20161210094349) do
     t.string   "formatted_address"
     t.string   "long_name"
     t.string   "address_components"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
