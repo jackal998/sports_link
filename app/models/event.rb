6716f53579395a053ae45b1143acba2178a1a356
jackal998 <jackal998@gmail.com>
2016-12-12 17:46:44 +0800

    models_relations(event,place,user)

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,6 @@
 class Event < ApplicationRecord
+  belongs_to :place
+  belongs_to :user
+
+  validates_presence_of :start_at
 end
--- app/models/place.rb
+++ app/models/place.rb
@@ -1,2 +1,9 @@
 class Place < ApplicationRecord
+  has_many :events, :dependent => :destroy
+
+  validates_presence_of :name
+  validates_presence_of :latitude
+  validates_presence_of :longitude
+  validates_uniqueness_of :long_name
+
 end
