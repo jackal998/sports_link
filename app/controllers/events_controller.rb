db4724d3175403e69e1d0b5a458ba7d8671ce987
jackal998 <jackal998@gmail.com>
2016-12-15 18:11:15 +0800

    Naming correction delete permitted_params

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -18,19 +18,6 @@ class EventsController < ApplicationController
     else
       redirect_to events_path
     end
-    # permitted_params = place_params
-    # @place = Place.find_by_place_id(permitted_params['place_id'])
-    # unless @place
-    #   @place = Place.new(
-    #     :place_id=> permitted_params['place_id'],
-    #     :name=> permitted_params['address_components0']['short_name'],
-    #     :long_name=> permitted_params['address_components0']['long_name'],
-    #     :latitude=> permitted_params['latitude'],
-    #     :longitude=> permitted_params['longitude'],
-    #     :formatted_address=> permitted_params['formatted_address']
-    #     )
-    #   @place.save
-    # end
   end
 
   def update
@@ -49,23 +36,4 @@ class EventsController < ApplicationController
   def find_event
     @event = Event.find(params[:id])
   end
-  # def place_params
-  #   info = {}
-  #   params[:params_to_post].each_pair do |key, information| 
-  #     information.each_pair do |title, value|
-  #       if title == "address_components"
-  #         value.each_pair do |component_index,component|
-  #           cmpnt = {}
-  #           component.each_pair do |component_title, component_value|
-  #             cmpnt.store( component_title , component_value ) unless component_index == 0
-  #           end
-  #           info.store("address_components#{component_index}", cmpnt )
-  #         end
-  #       else
-  #         info.store(title, value)
-  #       end
-  #     end
-  #   end
-  #   return info
-  # end
 end
