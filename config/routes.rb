953c41f579f1f5d375a274f0e7e415e84d38ca5e
jackal998 <jackal998@gmail.com>
2016-12-17 23:21:40 +0800

    routes for api resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  post '/rate' => 'rater#create', :as => 'rate'
 
   namespace :api, path: '' do
     constraints(host: 'api.localhost') do
       resources :users
+      resources :places
+      resources :events
     end
     constraints(host: 'api.just.got-game.org') do
       resources :users
@@ -12,24 +13,9 @@ Rails.application.routes.draw do
 
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
-
   resources :places
 
-  # devise_scope :user do
-  #   authenticated :user do
-  #     root 'events#index', as: :authenticated_root
-  #   end
+  resources :events
 
-  #   unauthenticated do
-  #     root 'devise/sessions#new', as: :unauthenticated_root
-  #   end
-  # end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :events do
-    collection do
-      get :match
-    end
-  end
-  # root to: "devise/sessions#new"
   root :to => "places#index"
 end
