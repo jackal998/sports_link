a728a8b259ba66be8d8b1161da1713881e565e11
jackal998 <jackal998@gmail.com>
2016-12-17 18:15:35 +0800

    place_id_distance_js_links

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -3,7 +3,21 @@ class PlacesController < ApplicationController
   skip_before_action :authenticate_user!, :only => [:index, :create]
 
   def index
-    
+    @place = Place.find_by_place_id(params[:place_id])
+    if @place.present?
+      @events = @place.events
+      if @events
+        case params[:date]
+        when 'day_after'
+          @events = @events.where("start_at > ? and start_at < ?", Date.today + 2, Date.today + 3)
+        when 'tomorrow'
+          @events = @events.where("start_at > ? and start_at < ?", Date.today + 1, Date.today + 2)
+        else 
+          @events = @events.where("start_at > ? and start_at < ?", Time.now + 3600 * 8, Date.today + 1)
+        end
+      end
+      @event = @events.first
+    end
   end
 
   def create
@@ -32,8 +46,8 @@ class PlacesController < ApplicationController
     @events = @place.events
     if @events
       case params[:date]
-      when 'yesterday'
-        @events = @events.where("start_at > ? and start_at < ?", Date.today - 1, Date.today )
+      when 'day_after'
+        @events = @events.where("start_at > ? and start_at < ?", Date.today + 2, Date.today + 3)
       when 'tomorrow'
         @events = @events.where("start_at > ? and start_at < ?", Date.today + 1, Date.today + 2)
       else 
