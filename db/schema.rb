a2eae12333957619c954ca00f27551a5b1d389ca
hahahaallday <chrisc.alchemist@gmail.com>
2016-12-20 17:10:08 +0800

    footer call to action jquery done. css fixed.

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20161219093912) do
-
 
   create_table "average_caches", force: :cascade do |t|
     t.integer  "rater_id"
