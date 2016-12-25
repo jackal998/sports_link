9be5ef05f43bde97195d6c21a10a27b93b4f27ae
hahahaallday <chrisc.alchemist@gmail.com>
2016-12-25 22:43:53 +0800

    basic host page added. focus in bottom part without navbar and day_selection

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,7 @@
 class UsersController < ApplicationController
+	def show
+		@user = current_user	
+    @place = Place.find_by_place_id(params[:place_id]) if params[:place_id]    
+    @event = get_events_by_place_and_date(@place, params[:date]).first
+  end
 end
