c5d44af39b4be09bb4acc2f3b4edf59cfa724e74
jackal998 <jackal998@gmail.com>
2016-12-28 01:18:17 +0800

    api user action create deleted, quit_event test run

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -7,12 +7,26 @@ class Api::UsersController < Api::BaseController
     @attended_events = @user.attended_events.selected
   end
 
-  def create
-    @user = User.new(params.require(:user).permit(:email, :password))
-    if @user.save
-      render json: { user: @user }
+  # def create
+  #   @user = User.new(params.require(:user).permit(:email, :password))
+  #   if @user.save
+  #     render json: { user: @user }
+  #   else
+  #     render json: { errors: @user.errors.full_messages, :needed => { email: "string", password: "string"} }, status: 402
+  #   end
+  # end
+
+  def quit_event
+    if @event = Event.find(params[:event_id])
+      if current_user.events.find(@event)
+        @event.destroy
+        render json: { message: "event:" + params[:event_id] + "deleted"}
+      else
+        @event.event_attendees.find_by_user_id(current_user.id).destroy if current_user.attended_events.include?(@event)
+        render json: { message: "quit event:" + params[:event_id] }
+      end
     else
-      render json: { errors: @user.errors.full_messages, :needed => { email: "string", password: "string"} }, status: 402
+      render json: { errors: 'No event error', :needed => { event_id: "integer"} }, status: 400
     end
   end
 end
