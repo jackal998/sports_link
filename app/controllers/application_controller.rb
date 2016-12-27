4256c88bd0ae870dd886f7eeebc47531d271ebe1
jackal998 <jackal998@gmail.com>
2016-12-28 01:12:53 +0800

    move get_events_by_place_and_date to app_helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,18 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
 
-  private
-
-  def get_events_by_place_and_date(place, date)
-    return [] unless place
-    case date
-      when 'day_after'
-        place.events.during( (Date.today + 2).beginning_of_day, (Date.today + 2).end_of_day)
-      when 'tomorrow'
-        place.events.during( (Date.today + 1).beginning_of_day, (Date.today + 1).end_of_day)
-      else 
-        place.events.during( Time.now, Date.today.end_of_day )
-    end
-  end
+  include ApplicationHelper
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -24,4 +24,15 @@ module ApplicationHelper
       return '約' + ((event.end_at - Time.now)/1.hour).round(0).to_s + '小時'
     end
   end
+  def get_events_by_place_and_date(place, date)
+    return [] unless place
+    case date
+      when 'day_after'
+        place.events.during((Date.today + 2).beginning_of_day, (Date.today + 2).end_of_day)
+      when 'tomorrow'
+        place.events.during((Date.today + 1).beginning_of_day, (Date.today + 1).end_of_day)
+      else 
+        place.events.during(Time.now, Date.today.end_of_day)
+    end
+  end
 end
