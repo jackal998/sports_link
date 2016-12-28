075983ce0dc3b310f012ee943e9b9cae2c2e08cd
jackal998 <jackal998@gmail.com>
2016-12-28 13:38:44 +0800

    api event controller remove index and valid end time before save

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -3,9 +3,9 @@ class Api::EventsController < Api::BaseController
   before_action :find_event, :only => [:join, :show]
   before_action :find_place, :only => [:new, :create, :update]
 
-  def index
-    render json: { errors: 'Oops! something is broken, We\'ll soon fix it!'}, :status => 400
-  end 
+  # def index
+  #   render json: { errors: 'Oops! something is broken, We\'ll soon fix it!'}, :status => 400
+  # end 
 
   def new
     @place = Place.find_by_place_id(params[:place_id])
@@ -13,12 +13,21 @@ class Api::EventsController < Api::BaseController
   end 
 
   def create
+    start_at, end_at = params[:start_at], params[:end_at]
+
     @event = Event.new(
-      place: @place, user: current_user, start_at: params[:start_at], end_at: params[:end_at],
+      place: @place, user: current_user, start_at: start_at, end_at: end_at,
       characteristic_of_user: params[:characteristic_of_user])
-    unless @event.save
-      render json: { errors: @event.errors.full_messages }, status: 402
+    if (end_at - start_at) > 0
+      @event = Event.new(place: @place, user: current_user, start_at: start_at, end_at: end_at, characteristic_of_user: params[:event][:characteristic_of_user])
+      current_user.join_event(@event)
+      unless @event.save
+        render json: { errors: @event.errors.full_messages }, status: 402
+      end
+    else
+      render json: { errors: '結束時間需晚於開始時間！，新增失敗' }, status: 400
     end
+
   end
 
   def join
