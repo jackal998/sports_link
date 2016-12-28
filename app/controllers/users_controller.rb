f48aa4d577fa3b54f4aaa0a125a75d373e24c5c8
jackal998 <jackal998@gmail.com>
2016-12-28 14:44:52 +0800

    user_controller show hosted_event and attended_events

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
 	def show
-		@user = current_user	
-    @place = Place.find_by_place_id(params[:place_id]) if params[:place_id]    
-    @event = get_events_by_place_and_date(@place, params[:date]).first
+    @user = current_user
+    @hosted_events = @user.events.selected
+    @attended_events = @user.attended_events.selected
+    # @place = Place.find_by_place_id(params[:place_id]) if params[:place_id]    
+    # @event = get_events_by_place_and_date(@place, params[:date]).first
   end
 end
