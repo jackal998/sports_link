30d2b0ac507b6bbb47822691617483a5f9bbb331
jackal998 <jackal998@gmail.com>
2016-12-30 11:05:57 +0800

    User show 'calender' controller => filter date view => add partial events_table avatar centered js => day_selection host_events attended_events

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,10 @@
 class UsersController < ApplicationController
 	def show
     @user = current_user
-    @hosted_events = @user.events
-    @attended_events = @user.attended_events
+    @hosted_events = @user.events.includes(:place)
+    @attended_events = @user.attended_events.where.not(user: @user).includes(:place)
+
+    @hosted_events = get_events_by_date(@hosted_events, params[:date])
+    @attended_events = get_events_by_date(@attended_events, params[:date])
   end
 end
