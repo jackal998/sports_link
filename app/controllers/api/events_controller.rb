ce8cae8964ff2fd8ce210e3bc6d4cc0a0eaccc0f
EJ <EJ@EJde-MacBook-Air.local>
2017-01-01 13:08:25 +0800

    @event find_event bug

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -44,7 +44,7 @@ class Api::EventsController < Api::BaseController
     end 
   end
   def find_event
-    @event = Event.find_by_id(params[:id]).includes(:event_attendees) if params[:id].present?
+    @event = Event.find_by_id(params[:id]) if params[:id].present?
     render json: { errors: 'No Evnet Error', needed: {id: 'integer'}}, :status => 400 unless @event
   end
 end
